ef544cc6632c94e703a0ad968621ac044c941877
P <p321654987@outlook.com>
2017-04-18 17:26:10 -0300

    spritesheet test 4

--- com/gamebase/chars/heroes/Knight.ts
+++ com/gamebase/chars/heroes/Knight.ts
@@ -39,7 +39,7 @@ module GameBase {
             
             // animation
             var aniSprite = this.addAnimation(this.game.add.sprite(0, 0, 'char'+this.identification+'-idle'), 'iddle');
-            aniSprite.y+=28; // padding sprite adjust
+            // aniSprite.y+=28; // padding sprite adjust
 
             // this.animationIdle.play(10, true);
 
--- dist/js/app.js
+++ dist/js/app.js
@@ -541,7 +541,7 @@ var GameBase;
             this.load.spritesheet('char1-idle', 'assets/default/images/chars/heroes/1/idle.png', 200, 300, 1);
             this.load.spritesheet('char2-idle', 'assets/default/images/chars/heroes/2/idle.png', 150, 200, 1);
             this.load.spritesheet('char3-idle', 'assets/default/images/chars/heroes/3/idle.png', 150, 250, 1);
-            this.load.spritesheet('char4-idle', 'assets/default/images/chars/heroes/4/iddle.png', 211, 203.2, 20);
+            this.load.spritesheet('char4-idle', 'assets/default/images/chars/heroes/4/iddle.png', 211, 190, 20);
             // icons
             this.load.image('heath-icon', 'assets/default/images/ui/ico-health.png');
             this.load.image('stamina-icon', 'assets/default/images/ui/ico-stamina.png');
@@ -936,7 +936,7 @@ var GameBase;
             _super.prototype.create.call(this);
             // animation
             var aniSprite = this.addAnimation(this.game.add.sprite(0, 0, 'char' + this.identification + '-idle'), 'iddle');
-            aniSprite.y += 28; // padding sprite adjust
+            // aniSprite.y+=28; // padding sprite adjust
             // this.animationIdle.play(10, true);
             this.playAnimation('iddle', 10);
         };
