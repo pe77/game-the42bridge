ddc960687bc3cb9330851ff075c3d895040dfa55
P <p321654987@outlook.com>
2017-04-20 15:31:11 -0300

    tsc

--- dist/js/app.js
+++ dist/js/app.js
@@ -11,6 +11,370 @@ var __extends = (this && this.__extends) || (function () {
 window.onload = function () {
     var game = new GameBase.Game();
 };
+/// <reference path='../vendor/phaser/phaser.d.ts' />
+var Pk;
+(function (Pk) {
+    var PkEvent = (function () {
+        function PkEvent(name, target) {
+            this.id = ++PkEvent.id;
+            this.listeners = [];
+            this.target = target;
+            this.name = name;
+        }
+        PkEvent.prototype.add = function (key, callBack, context) {
+            var context = context || {};
+            var exist = false;
+            // verifica se já não foi add
+            for (var i = 0; i < this.listeners.length; i++) {
+                if (this.listeners[i].callBack.toString() === callBack.toString()) {
+                    exist = true;
+                    break;
+                }
+            }
+            ;
+            if (!exist)
+                this.listeners.push({ key: key, callBack: callBack, context: context });
+            //
+        };
+        PkEvent.prototype.dispatch = function (key) {
+            var args = [];
+            for (var _i = 1; _i < arguments.length; _i++) {
+                args[_i - 1] = arguments[_i];
+            }
+            for (var i = 0; i < this.listeners.length; i++) {
+                if (key == this.listeners[i].key) {
+                    var data = {
+                        target: this.target // ho dispatch the event
+                    };
+                    // se houver contexto, manda pelo contexto
+                    if (this.listeners[i].context) {
+                        (_a = this.listeners[i].callBack).call.apply(_a, [this.listeners[i].context, data].concat(args));
+                        return;
+                    }
+                    // dispara sem contexto mesmo
+                    (_b = this.listeners[i]).callBack.apply(_b, [data].concat(args));
+                }
+            }
+            var _a, _b;
+        };
+        return PkEvent;
+    }());
+    PkEvent.id = 0;
+    Pk.PkEvent = PkEvent;
+})(Pk || (Pk = {}));
+/// <reference path='../PkTransition.ts' />
+var Pk;
+(function (Pk) {
+    var PkTransitionAnimation;
+    (function (PkTransitionAnimation) {
+        var Default = (function () {
+            function Default() {
+                this.event = new Pk.PkEvent('PkTADefault', this);
+            }
+            Default.prototype.start = function () {
+                // animation here
+                // ...
+                this.event.dispatch(Pk.E.OnTransitionEndStart);
+            };
+            Default.prototype.end = function () {
+                // animation here
+                // ...
+                this.event.dispatch(Pk.E.OnTransitionEndEnd);
+            };
+            return Default;
+        }());
+        PkTransitionAnimation.Default = Default;
+    })(PkTransitionAnimation = Pk.PkTransitionAnimation || (Pk.PkTransitionAnimation = {}));
+})(Pk || (Pk = {}));
+/// <reference path='../event/PkEvent.ts' />
+/// <reference path='../PkGame.ts' />
+/// <reference path='PkState.ts' />
+/// <reference path='transitions/Default.ts' />
+var Pk;
+(function (Pk) {
+    var PkTransition = (function () {
+        function PkTransition(state) {
+            this.transitionAnimation = new Pk.PkTransitionAnimation.Default();
+            // defaults
+            this.clearWorld = true;
+            this.clearCache = false;
+            this.game = state.game;
+            this.state = state;
+        }
+        PkTransition.prototype.change = function (to) {
+            var args = [];
+            for (var _i = 1; _i < arguments.length; _i++) {
+                args[_i - 1] = arguments[_i];
+            }
+            this.to = to;
+            this.params = args;
+            this.transitionAnimation.event.add(Pk.E.OnTransitionEndStart, this.endStartAnimation, this);
+            this.transitionAnimation.event.add(Pk.E.OnTransitionEndEnd, this.endStartAnimation, this);
+            this.transitionAnimation.start();
+        };
+        // This is called when the state preload has finished and creation begins
+        PkTransition.prototype.endStartAnimation = function (e) {
+            var _this = this;
+            var args = [];
+            for (var _i = 1; _i < arguments.length; _i++) {
+                args[_i - 1] = arguments[_i];
+            }
+            this.game.state.onStateChange.addOnce(function (state) {
+                // get current state
+                var currentState = _this.game.state.getCurrentState();
+                _this.game.state.onCreateCallback = function () {
+                    // call current state create
+                    currentState.create();
+                    // play transition end
+                    _this.transitionAnimation.end();
+                };
+            });
+            // change state
+            (_a = this.game.state).start.apply(_a, [this.to, this.clearWorld, this.clearCache].concat(this.params));
+            var _a;
+        };
+        return PkTransition;
+    }());
+    Pk.PkTransition = PkTransition;
+    var E;
+    (function (E) {
+        E.OnTransitionEndStart = "OnTransitionEndStart";
+        E.OnTransitionEndEnd = "OnTransitionEndEnd";
+    })(E = Pk.E || (Pk.E = {}));
+})(Pk || (Pk = {}));
+/// <reference path='../vendor/phaser/phaser.d.ts' />
+var Pk;
+(function (Pk) {
+    var PkElement = (function (_super) {
+        __extends(PkElement, _super);
+        function PkElement(game) {
+            var _this = _super.call(this, game) || this;
+            _this.id = ++PkElement.id;
+            _this.tweens = [];
+            _this.name = "PkElement-" + _this.id;
+            // inicia gerenciador de eventos
+            _this.event = new Pk.PkEvent('element-event-' + _this.id, _this);
+            return _this;
+        }
+        PkElement.prototype.addTween = function (displayObject) {
+            this.tweens.push(this.game.add.tween(displayObject));
+            return this.tweens[this.tweens.length - 1];
+        };
+        PkElement.prototype.destroy = function () {
+            for (var i = this.tweens.length - 1; i >= 0; i--) {
+                this.tweens[i].stop();
+            }
+            _super.prototype.destroy.call(this);
+        };
+        return PkElement;
+    }(Phaser.Group));
+    PkElement.id = 0;
+    Pk.PkElement = PkElement;
+})(Pk || (Pk = {}));
+/// <reference path='PkTransition.ts' />
+/// <reference path='../element/PkElement.ts' />
+/// <reference path='../vendor/phaser/phaser.d.ts' />
+var Pk;
+(function (Pk) {
+    var PkState = (function (_super) {
+        __extends(PkState, _super);
+        function PkState() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.layers = [];
+            _this.addLayer = function (layerName) {
+                var exist = false;
+                // check if already exist
+                for (var i = 0; i < this.layers.length; i++) {
+                    if (this.layers[i].name == layerName) {
+                        exist = true;
+                        break;
+                    }
+                }
+                ;
+                if (!exist) {
+                    // add to layer
+                    this.layers.push({
+                        name: layerName,
+                        total: 0,
+                        group: this.game.add.group()
+                    });
+                }
+            };
+            _this.addToLayer = function (layerName, element) {
+                var exist = false;
+                // check if already exist
+                for (var i = 0; i < this.layers.length; i++) {
+                    if (this.layers[i].name == layerName) {
+                        exist = true;
+                        break;
+                    }
+                }
+                ;
+                // if dont exist, wharever
+                if (!exist)
+                    return;
+                //
+                // add element to layer
+                this.layers[i].group.add(element);
+                this.layers[i].total = this.layers[i].group.total;
+                // order layers
+                for (var i = 0; i < this.layers.length; i++)
+                    this.game.world.bringToTop(this.layers[i].group);
+                //
+            };
+            return _this;
+        }
+        PkState.prototype.getGame = function () {
+            return this.game;
+        };
+        PkState.prototype.init = function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            this.transition = new Pk.PkTransition(this);
+        };
+        PkState.prototype.create = function () {
+            // console.log('PkState create');
+        };
+        return PkState;
+    }(Phaser.State));
+    Pk.PkState = PkState;
+})(Pk || (Pk = {}));
+/// <reference path='vendor/phaser/phaser.d.ts' />
+/// <reference path='state/PkState.ts' />
+var Pk;
+(function (Pk) {
+    var PkGame = (function (_super) {
+        __extends(PkGame, _super);
+        function PkGame(pkConfig) {
+            if (pkConfig === void 0) { pkConfig = new Pk.PkConfig(); }
+            var _this = _super.call(this, pkConfig.canvasSize[0], pkConfig.canvasSize[1], pkConfig.renderMode, pkConfig.canvasId) || this;
+            PkGame.pkConfig = pkConfig;
+            // add states
+            _this.state.add('PkLoaderPreLoader', PkGame.pkConfig.preLoaderState);
+            // init loader
+            _this.state.start('PkLoaderPreLoader');
+            PkGame.game = _this;
+            return _this;
+        }
+        return PkGame;
+    }(Phaser.Game));
+    Pk.PkGame = PkGame;
+    var PkLoaderPreLoader = (function (_super) {
+        __extends(PkLoaderPreLoader, _super);
+        function PkLoaderPreLoader() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        PkLoaderPreLoader.prototype.init = function () {
+            // add loader screen
+            this.game.state.add('PkLoader', PkGame.pkConfig.loaderState);
+        };
+        PkLoaderPreLoader.prototype.preload = function () {
+            // load loadingbar sprite
+            this.load.image('pk-loading-bar', PkGame.pkConfig.loaderLoadingBar);
+        };
+        PkLoaderPreLoader.prototype.create = function () {
+            // change to preloader screen*
+            this.game.state.start('PkLoader');
+        };
+        return PkLoaderPreLoader;
+    }(Pk.PkState));
+    Pk.PkLoaderPreLoader = PkLoaderPreLoader;
+})(Pk || (Pk = {}));
+var Pk;
+(function (Pk) {
+    var PkConfig = (function () {
+        function PkConfig() {
+            this.canvasSize = [800, 600]; // width, height
+            this.canvasId = 'game';
+            this.renderMode = RenderMode.AUTO;
+            this.initialState = ''; // initial state after loadscreen
+            // loading settings
+            this.loaderLoadingBar = 'assets/states/loader/images/loading-bar.png'; // loading bar
+            this.loaderWaitingTime = 1000; // 1 sec
+            this.loaderState = Pk.PkLoader;
+            this.preLoaderState = Pk.PkLoaderPreLoader;
+        }
+        return PkConfig;
+    }());
+    Pk.PkConfig = PkConfig;
+    // for remember ...    :'(     ... never forget
+    var RenderMode;
+    (function (RenderMode) {
+        RenderMode[RenderMode["AUTO"] = Phaser.AUTO] = "AUTO";
+        RenderMode[RenderMode["CANVAS"] = Phaser.CANVAS] = "CANVAS";
+        RenderMode[RenderMode["WEBGL"] = Phaser.WEBGL] = "WEBGL";
+        RenderMode[RenderMode["HEADLESS"] = Phaser.HEADLESS] = "HEADLESS";
+    })(RenderMode = Pk.RenderMode || (Pk.RenderMode = {}));
+})(Pk || (Pk = {}));
+/// <reference path='state/PkState.ts' />
+var Pk;
+(function (Pk) {
+    var PkLoader = (function (_super) {
+        __extends(PkLoader, _super);
+        function PkLoader() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        PkLoader.prototype.init = function () {
+        };
+        PkLoader.prototype.preload = function () {
+            this.load.setPreloadSprite(this.add.sprite(200, 250, 'pk-loading-bar'));
+        };
+        PkLoader.prototype.create = function () {
+            var _this = this;
+            setTimeout(function () {
+                // if initial state set, load
+                if (Pk.PkGame.pkConfig.initialState != '')
+                    _this.game.state.start(Pk.PkGame.pkConfig.initialState);
+                //
+            }, Pk.PkGame.pkConfig.loaderWaitingTime);
+        };
+        return PkLoader;
+    }(Pk.PkState));
+    Pk.PkLoader = PkLoader;
+})(Pk || (Pk = {}));
+/// <reference path='../vendor/phaser/phaser.d.ts' />
+var Pk;
+(function (Pk) {
+    var PkUtils = (function () {
+        function PkUtils() {
+        }
+        // check if is a empty object
+        PkUtils.isEmpty = function (obj) {
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop))
+                    return false;
+            }
+            return true && JSON.stringify(obj) === JSON.stringify({});
+        };
+        PkUtils.createSquareBitmap = function (game, width, height, color) {
+            if (color === void 0) { color = "#000000"; }
+            var bmd = game.add.bitmapData(width, height);
+            bmd.ctx.beginPath();
+            bmd.ctx.rect(0, 0, width, height);
+            bmd.ctx.fillStyle = color;
+            bmd.ctx.fill();
+            return bmd;
+        };
+        PkUtils.createSquare = function (game, width, height, color) {
+            if (color === void 0) { color = "#000000"; }
+            var bmd = Pk.PkUtils.createSquareBitmap(game, width, height, color);
+            return game.add.sprite(0, 0, bmd);
+        };
+        return PkUtils;
+    }());
+    Pk.PkUtils = PkUtils;
+})(Pk || (Pk = {}));
+/// <reference path='PkGame.ts' />
+/// <reference path='PkConfig.ts' />
+/// <reference path='PkLoader.ts' />
+/// <reference path='state/PkState.ts' />
+/// <reference path='state/PkTransition.ts' />
+/// <reference path='state/transitions/Default.ts' />
+/// <reference path='event/PkEvent.ts' />
+/// <reference path='element/PkElement.ts' />
+/// <reference path='utils/PkUtils.ts' /> 
 /// <reference path='../pkframe/refs.ts' />
 var GameBase;
 (function (GameBase) {
@@ -1411,4 +1775,67 @@ var GameBase;
     GameBase.SelectedIcon = SelectedIcon;
 })(GameBase || (GameBase = {}));
 /// <reference path='../../../pkframe/refs.ts' />
+var Pk;
+(function (Pk) {
+    var PkLayer = (function (_super) {
+        __extends(PkLayer, _super);
+        function PkLayer() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.distance = 1; // use for parallax effect
+            return _this;
+        }
+        return PkLayer;
+    }(Pk.PkElement));
+    Pk.PkLayer = PkLayer;
+})(Pk || (Pk = {}));
+/// <reference path='../event/PkEvent.ts' />
+/// <reference path='../PkGame.ts' />
+/// <reference path='PkLayer.ts' />
+var Pk;
+(function (Pk) {
+    var PkParallax = (function () {
+        function PkParallax(state) {
+            this.layers = [];
+            this.state = state;
+        }
+        PkParallax.prototype.add = function (element, distance, cameraLock) {
+            if (cameraLock === void 0) { cameraLock = true; }
+            // if using TileSprite, distance is mandatary
+            if (element instanceof Phaser.TileSprite && !distance)
+                throw new Error("If you use TileSprite for parallax, distance param is mandatory");
+            //
+            if (element instanceof Pk.PkLayer && distance)
+                element.distance = distance;
+            //
+            if (element instanceof Pk.PkLayer && distance)
+                element.distance = distance;
+            //
+            if (element instanceof Phaser.TileSprite && cameraLock)
+                element.fixedToCamera = true;
+            //
+            this.layers.push({
+                tileElement: element instanceof Phaser.TileSprite ? element : null,
+                layerElement: element instanceof Pk.PkLayer ? element : null,
+                distance: element instanceof Pk.PkLayer ? element.distance : distance
+            });
+        };
+        PkParallax.prototype.update = function () {
+            for (var i in this.layers) {
+                // if is tile sprite element
+                if (this.layers[i].tileElement) {
+                    var posX = 1 / this.layers[i].distance;
+                    this.layers[i].tileElement.tilePosition.x = -this.state.game.camera.x * posX;
+                    this.layers[i].tileElement.tilePosition.y = -this.state.game.camera.y * posX;
+                }
+                // if is layer
+                if (this.layers[i].layerElement) {
+                    // @todo
+                }
+            }
+            ;
+        };
+        return PkParallax;
+    }());
+    Pk.PkParallax = PkParallax;
+})(Pk || (Pk = {}));
 //# sourceMappingURL=app.js.map
